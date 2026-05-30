331577e72784f70cede213a09d16234c3e601431
bhuvan <bhuvanannappa@gmail.com>
2026-05-30 17:18:06 +0530

    feat: assemble Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Hero from '../sections/Hero';
+import About from '../sections/About';
+import Services from '../sections/Services';
+import WorkPreview from '../sections/WorkPreview';
+import Contact from '../sections/Contact';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Services />
+      <WorkPreview />
+      <Contact />
+    </>
+  );
+}
